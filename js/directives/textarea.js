894b6665151ccf40c63823dafad697b2643175c2
matt-manywho <matt.bates@manywho.com>
2015-01-06 14:28:31 +0000

    Added label to textarea component

--- js/directives/textarea.js
+++ js/directives/textarea.js
@@ -8,7 +8,17 @@ manywho.directive('mwTextarea', ['$compile', 'engine', 'model', 'viewBuilder', f
         },
         link: function (scope, element, attrs) {
 
-            var html = '<textarea id="{{id}}" />';
+            var component = model.getComponent(scope.id);
+            var classes = viewBuilder.getClasses(scope.parent);
+
+            var html = '<div class="form-group ' + classes + '">';
+
+            if (component.label && component.label.trim().length > 0) {
+                html += '<label for="{{id}}">' + component.label + '</label>';
+            }
+            
+            html += '<textarea id="{{id}}" class="form-control"/>';
+            html += '</div>';
 
             var compiledElement = $compile(html)(scope);
             element.replaceWith(compiledElement);
