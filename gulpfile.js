a868bf15e69a5407ec5c1b8a4fb0b6db8e64429d
matt-manywho <matt.bates@manywho.com>
2015-01-06 11:59:20 +0000

    Added initial gulpfile with browser-sync support

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+    browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function () {
+
+    var files = [
+        '*.html',
+        'css/**/*.css',
+        'img/**/*.png',
+        'js/**/*.js'
+    ];
+
+    browserSync.init(files, {
+        server: {
+            baseDir: '.'
+        }
+    });
+
+});
