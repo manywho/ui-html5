6930c296b8c4754ca799df60c9ad2c13225c3d1f
matt-manywho <matt.bates@manywho.com>
2015-02-02 13:00:32 +0000

    Removed developerName from vertical component rendering

--- js/components/vertical.js
+++ js/components/vertical.js
@@ -9,7 +9,7 @@
             var classes = manywho.styling.getClasses(this.props.parentId, this.props.id, "vertical_flow");
             var children = manywho.model.getChildren(this.props.id);
             
-            return React.createElement('div', { className: classes, id: manywho.model.getContainer(this.props.id).developerName }, manywho.component.getChildComponents(children, this.props.id));
+            return React.createElement('div', { className: classes }, manywho.component.getChildComponents(children, this.props.id));
 
         }
 
