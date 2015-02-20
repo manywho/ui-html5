5457772b79a08e12872733dd30867347a580b445
matt-manywho <matt.bates@manywho.com>
2015-02-20 16:11:12 +0000

    Removed bottom margin from an empty main container

--- js/components/main.js
+++ js/components/main.js
@@ -20,11 +20,19 @@
 
             }
 
+            var componentElements = manywho.component.getChildComponents(children, this.props.id, this.props.flowKey);
+            var outcomeElements = manywho.component.getOutcomes(outcomes, this.props.flowKey);
+
+            var classNames = [
+                'container main',
+                (componentElements.length + outcomeElements.length == 0) ? 'main-empty' : ''
+            ].join(' ');
+
             return React.DOM.div({ className: 'full-height' }, [
                         React.createElement(manywho.component.getByName('navigation'), { id: manywho.model.getDefaultNavigationId(this.props.flowKey), flowKey: this.props.flowKey }),
-                        React.DOM.div({ className: 'container main' }, [
-                            manywho.component.getChildComponents(children, this.props.id, this.props.flowKey),
-                            manywho.component.getOutcomes(outcomes, this.props.flowKey)
+                        React.DOM.div({ className: classNames }, [
+                            componentElements,
+                            outcomeElements
                         ]),
                         modal,
                         React.createElement(manywho.component.getByName('wait'), { wait: wait })
