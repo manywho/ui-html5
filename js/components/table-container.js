ae34e3826264130e05f33a759b5311a7d0fb939b
matt-manywho <matt.bates@manywho.com>
2015-03-27 15:07:27 +0000

    Reverted behaviour where table rows could not be selected if no bulk actions were defined, this functionality is required as it should still be possible to select a row then hit a page outcome

--- js/components/table-container.js
+++ js/components/table-container.js
@@ -126,13 +126,6 @@
 
         onRowClicked: function (e) {
 
-            if (!areBulkActionsDefined(this.outcomes)) {
-
-                // Don't select the row if there aren't any bulk actions defined
-                return;
-
-            }
-
             var selectedRows = this.state.selectedRows;
 
             if (selectedRows.indexOf(e.currentTarget.id) == -1) {
