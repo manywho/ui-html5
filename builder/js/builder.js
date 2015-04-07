4ed08352e40560763c604945dd2862d6e411cfc6
Joao Moreira <joao.moreira@manywho.com>
2015-04-07 17:01:01 +0100

    Template for the builder component

--- builder/js/builder.js
+++ builder/js/builder.js
@@ -1,5 +1,21 @@
-manywho.builder = (function () {
+(function (manywho) {
+
+    var builder = React.createClass({
+
+        handleChange: function (e, args) {
 
 
 
-})();
+        },
+
+        render: function () {
+
+
+
+        }
+
+    });
+
+    manywho.component.register('builder', builder);
+
+})(manywho);
