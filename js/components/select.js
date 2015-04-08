2ad00d8f1ebe7883bef4293ad02fe470e8095cf5
Joao Moreira <joao.moreira@manywho.com>
2015-04-08 13:47:00 +0100

    CORE-660 - Fixed application of invalid selected attributes on multi select boxes

--- js/components/select.js
+++ js/components/select.js
@@ -24,7 +24,15 @@ permissions and limitations under the License.
             var isSelected = item.isSelected
                 || (this.state && this.state.objectData && this.state.objectData.length > 0 && manywho.utils.isEqual(this.state.objectData[0].externalId, item.externalId, true));
 
-            return React.DOM.option({ value: item.externalId, selected: isSelected ? 'selected' : '' }, label.contentValue);
+            var attributes = { value: item.externalid };
+
+            if (isSelected) {
+
+                attributes.selected = 'selected'
+
+            }
+
+            return React.DOM.option(attributes, label.contentValue);
 
         }
 
