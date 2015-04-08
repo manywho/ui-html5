5668d168146e2b9663518f8eaaf996659a4dbae3
matt-manywho <matt.bates@manywho.com>
2015-04-08 12:00:49 +0100

    CORE-664 - Added validation state rendering to textarea components

--- js/components/textarea.js
+++ js/components/textarea.js
@@ -35,7 +35,8 @@ permissions and limitations under the License.
                 maxLength: model.maxSize,
                 onChange: this.handleChange,
                 cols: model.width,
-                rows: model.height
+                rows: model.height,
+                className: 'form-control'
             };
 
             if (!model.isEnabled) {
