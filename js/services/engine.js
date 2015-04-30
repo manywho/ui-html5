472e09bd7a4f7296acae2c77c0e8a91808c55dc1
Joao Moreira <joao.moreira@manywho.com>
2015-04-30 09:51:41 +0100

    CORE-717 - Removed unused 'kind' parameter in the onAuthorizationFailed and invokeAuthorization functions.

--- js/services/engine.js
+++ js/services/engine.js
@@ -56,7 +56,7 @@ manywho.engine = (function (manywho) {
 
     }
 
-    function onAuthorizationFailed(response, flowKey, kind, callback) {
+    function onAuthorizationFailed(response, flowKey, callback) {
 
         if (manywho.state.getSessionData(flowKey) != null) {
 
@@ -65,7 +65,7 @@ manywho.engine = (function (manywho) {
         } else {
 
             // Authorization failed, retry
-            manywho.authorization.invokeAuthorization(response, flowKey, kind, callback);
+            manywho.authorization.invokeAuthorization(response, flowKey, callback);
 
         }
 
