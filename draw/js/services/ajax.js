970d46be8fd617d69da4051660653ace1fbba9b4
Joao Moreira <joao.moreira@manywho.com>
2015-05-08 16:58:44 +0100

    Replaced the converter url

--- draw/js/services/ajax.js
+++ draw/js/services/ajax.js
@@ -268,7 +268,7 @@ manywho.draw.ajax = (function () {
         convertLua: function (metadata) {
 
             return $.ajax({
-                url: 'http://3c5ce02.ngrok.com/api/corvisa/1/convert',
+                url: 'http://manywho-corvisa-ivr-converter.elasticbeanstalk.com/api/corvisa/1/convert',
                 type: 'POST',
                 dataType: 'json',
                 contentType: 'application/json',
