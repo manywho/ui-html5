101627e637a1dbc6b9a150cee15e1961c4150c82
matt-manywho <matt.bates@manywho.com>
2015-07-08 10:08:33 +0100

    CORE-880 - Changed the status spinner to use the new CSS3 wait animation

--- runtime/js/components/main.js
+++ runtime/js/components/main.js
@@ -83,7 +83,6 @@ permissions and limitations under the License.
 
             var classNames = [
                 'main',
-                (componentElements.length + outcomeElements.length == 0) ? 'main-empty' : '',
                 (isFullWidth) ? 'container-fluid full-width' : 'container',
                 (manywho.settings.isDebugEnabled(this.props.flowKey)) ? 'main-debug' : ''
             ];
