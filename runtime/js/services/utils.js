8dadf07e3b054fd23e2885e64483d3238af75338
Joao Moreira <joao.moreira@manywho.com>
2015-12-01 11:15:15 +0000

    CORE-1298 - Removing a Flow now checks if collaboration is enabled.

--- runtime/js/services/utils.js
+++ runtime/js/services/utils.js
@@ -515,8 +515,14 @@ manywho.utils = (function (manywho, $) {
             manywho.settings.remove(flowKey);
             manywho.state.remove(flowKey);
             manywho.social.remove(flowKey);
-            manywho.collaboration.leave('Another user', flowKey);
-            manywho.collaboration.remove(flowKey);
+
+            if (manywho.settings.flow('collaboration.isEnabled', flowKey)) {
+
+                manywho.collaboration.leave('Another user', flowKey);
+                manywho.collaboration.remove(flowKey);
+
+            }
+
             manywho.callbacks.remove(flowKey);
             manywho.utils.removeFlowFromDOM(flowKey);
 
