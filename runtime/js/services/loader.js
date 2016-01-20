b8db00d1d2daeec53db491a5fc2c03ee3f10ca35
matt-manywho <matt.bates@manywho.com>
2016-01-20 11:54:10 +0000

    CORE-1479 - Fixed callback never being called if no custom hashes were provided

--- runtime/js/services/loader.js
+++ runtime/js/services/loader.js
@@ -112,24 +112,31 @@ permissions and limitations under the License.
 
         var loadedScriptCount = 0;
 
-        scripts.forEach(function(url, index, scripts) {
+        if (scripts && scripts.length > 0) {
 
-            appendScript(url, function() {
+            scripts.forEach(function(url, index, scripts) {
 
-                loadedScriptCount++;
-                if (loadedScriptCount == scripts.length) {
+                appendScript(url, function() {
 
-                    callback();
+                    loadedScriptCount++;
+                    if (loadedScriptCount == scripts.length) {
 
-                }
+                        callback();
+
+                    }
+
+                });
 
             });
 
-        });
+        }
+        else {
+            callback();
+        }
 
     }
 
-    function loadFromHashes(hashes, customResources, cdnUrl, initialTheme, callback) {
+    function loadFromHashes(hashes, cdnUrl, initialTheme, callback) {
 
         var hashesCount = 0;
         var scripts = [];
@@ -154,10 +161,6 @@ permissions and limitations under the License.
                             if (!document.getElementById('theme'))
                                 appendStylesheet(initialTheme || (cdnUrl + '/css/themes/mw-paper.css'), 'theme');
 
-                            var parsedCustomResources = parseCustomResources(customResources);
-                            parsedCustomResources.stylesheets.forEach(appendStylesheet);
-
-                            scripts = scripts.concat(parsedCustomResources.scripts);
                             loadScripts(scripts, callback);
 
                         }
@@ -172,7 +175,16 @@ permissions and limitations under the License.
             });
 
         }
+        else {
+            callback();
+        }
 
+    }
+
+    function loadCustomResources(customResources, callback) {
+        var parsed = parseCustomResources(customResources);
+        parsed.stylesheets.forEach(appendStylesheet);
+        loadScripts(parsed.scripts, callback);
     }
 
     manywho.loader = {
@@ -191,9 +203,11 @@ permissions and limitations under the License.
 
                         loadScriptsSequentially(vendorHashes.scripts, 0, function() {
 
-                            loadFromHashes([hashes[0]], customResources, cdnUrl, initialTheme, function() {
+                            loadFromHashes([hashes[0]], cdnUrl, initialTheme, function() {
                                 hashes.splice(0, 1);
-                                loadFromHashes(hashes, customResources, cdnUrl, initialTheme, callback);
+                                loadFromHashes(hashes, cdnUrl, initialTheme, function() {
+                                    loadCustomResources(customResources, callback);
+                                });
                             });
 
                         });
@@ -207,9 +221,11 @@ permissions and limitations under the License.
             }
             else {
 
-                loadFromHashes([hashes[0]], customResources, cdnUrl, initialTheme, function() {
+                loadFromHashes([hashes[0]], cdnUrl, initialTheme, function() {
                     hashes.splice(0, 1);
-                    loadFromHashes(hashes, customResources, cdnUrl, initialTheme, callback);
+                    loadFromHashes(hashes, cdnUrl, initialTheme, function() {
+                        loadCustomResources(customResources, callback);
+                    });
                 });
 
             }
