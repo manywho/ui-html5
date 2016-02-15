a9ce41b9747ef0fc7c2555a4abf46da1d905c72f
Joao Moreira <joao.moreira@manywho.com>
2016-02-15 09:46:22 +0000

    CORE-1568 - File upload component from feed no longer renders outcomes

--- runtime/js/components/feed.js
+++ runtime/js/components/feed.js
@@ -101,7 +101,7 @@ permissions and limitations under the License.
             var fileUpload = null;
             if (this.props.isAttachmentsEnabled) {
 
-                fileUpload = React.createElement(manywho.component.getByName('file-upload'), { flowKey: this.props.flowKey, multiple: true, upload: manywho.social.attachFiles, smallInputs: true, isUploadVisible: false, browseCaption: 'Attach Files', ref: 'files' });
+                fileUpload = React.createElement(manywho.component.getByName('file-upload'), { flowKey: this.props.flowKey, multiple: true, upload: manywho.social.attachFiles, smallInputs: true, isChildComponent: true, isUploadVisible: false, id: this.props.id, browseCaption: 'Attach Files', ref: 'files' });
 
             }
 
