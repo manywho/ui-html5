c5e710cd05897a5cc7889e0dc20d6e5928c94bbe
matt-manywho <matt.bates@manywho.com>
2016-06-02 17:00:50 +0100

    CORE-1990 - Added logging to hash re-writing task

--- gulp-tasks/dist/hashes.js
+++ gulp-tasks/dist/hashes.js
@@ -3,10 +3,14 @@ module.exports = function(gulp, plugins) {
         gulp.src('dist/hashes.json')        
             .pipe(plugins.jsonEditor(function(hashes) {
                 for (hash in hashes) {
-                    if (hashes[hash].indexOf('.css') != -1)
-                        hashes[hash] = '/css/' + hashes[hash]
-                    else if (hashes[hash].indexOf('.js') != -1)
-                        hashes[hash] = '/js/' + hashes[hash]
+                    if (hashes[hash].indexOf('.css') != -1) {
+                        console.log('Re-writing hash ' + hashes[hash] + ' to ' + '/css/' + hashes[hash]);
+                        hashes[hash] = '/css/' + hashes[hash];
+                    }
+                    else if (hashes[hash].indexOf('.js') != -1) {
+                        console.log('Re-writing hash ' + hashes[hash] + ' to ' + '/js/' + hashes[hash]);
+                        hashes[hash] = '/js/' + hashes[hash];
+                    }
                 }
                 return hashes;
             }))
