63a625618882b24af86de2d8891a1d396a498ce5
Matt Bates <Matt Bates>
2016-09-13 15:48:38 +0100

    CORE-2611 - Inheriting chart interface, removed empty stylesheet

--- js/interfaces/IChartComponentProps.ts
+++ js/interfaces/IChartComponentProps.ts
@@ -1,26 +1,7 @@
 /// <reference path="../../typings/index.d.ts" />
-/// <reference path="IComponentProps.ts" />
+/// <reference path="IItemsComponentProps.ts" />
 
-interface IChartComponentProps extends IComponentProps {
-    id: string,
-    parentId: string,
-    flowKey: string,
-    isDesignTime: boolean,
-    contentElement: JSX.Element,
-    hasMoreResults: boolean,
-    onOutcome: Function,
-    select: Function,
-    selectAll: Function,
-    clearSelection: Function,
-    objectData: Array<any>,
-    onSearch: Function,
-    outcomes: Array<any>,
-    refresh: (MouseEvent) => void,
-    onNext: Function,
-    onPrev: Function,
-    page: number,
-    limit: number,
-    isLoading: boolean,
+interface IChartComponentProps extends IItemsComponentProps {
     type: string,
     options: any
 }
