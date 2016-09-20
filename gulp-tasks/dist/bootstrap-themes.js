693104e4a406960f53cdff5c395a1c080e255db5
Matt Bates <matt.bates@manywho.com>
2016-09-20 12:27:53 +0100

    CORE-2643 - Returning from gulp dist tasks for more accurate timing. Also re-shuffled dist take run sequence to make it more reliable

--- gulp-tasks/dist/bootstrap-themes.js
+++ gulp-tasks/dist/bootstrap-themes.js
@@ -1,6 +1,6 @@
 module.exports = function(gulp, plugins) {
     return function() {
-        gulp.src('css/themes/*.less')
+        return gulp.src('css/themes/*.less')
             .pipe(plugins.less())
             .pipe(plugins.replace('.mw-bs html {', '.mw-bs {'))
             .pipe(plugins.replace('.mw-bs body {', '.mw-bs {'))
