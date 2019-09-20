e7c218716b2cb8e5c158ad6da050a911a16b1baf
jamesbrattBoomi <james_bratt@dell.com>
2019-09-20 13:08:52 +0100

    FIxing merge conflict related to docker and gzip removal changes

--- gulpfile.js
+++ gulpfile.js
@@ -26,6 +26,7 @@ gulp.task('refresh', function () {
 gulp.task('dist-loader', function() {
     return gulp.src('js/loader.js')
         .pipe(plugins.uglify())
+        .pipe(plugins.gzip({ append: false }))
         .pipe(plugins.rename('loader.min.js'))
         .pipe(gulp.dest(`${argv.output_directory}/js`));
 });
@@ -40,36 +41,8 @@ gulp.task('dist-html', function () {
 
 gulp.task('dist-img', function() {
     return gulp.src('img/*.*')
-        .pipe(gulp.dest('./dist/img'));
+        .pipe(plugins.gzip({ append: false }))
+        .pipe(gulp.dest(`${argv.output_directory}/img`));
 });
-
-<<<<<<< HEAD
-// Test files (they are the same but have different urls and are not gzipped)
-
-gulp.task('test-loader', function() {
-    return gulp.src('js/loader.js')
-        .pipe(plugins.uglify())
-        .pipe(plugins.rename('loader.min.js'))
-        .pipe(gulp.dest('./test/js'));
-});
-
-gulp.task('test-html', function () {
-    return gulp.src('default.html')
-        .pipe(plugins.replace('{{cdnurl}}', argv.test_cdnurl))
-        .pipe(plugins.replace('{{platform_uri}}', argv.test_platform_uri))
-        .pipe(plugins.rename(argv.test_tenant + '.' + argv.player))
-        .pipe(gulp.dest('./test/players/'));
-});
-
-gulp.task('test-img', function() {
-    return gulp.src('img/*.*')
-        .pipe(gulp.dest('./test/img'));
-});
-
-
-
-gulp.task('build-dist', ['dist-loader', 'dist-html', 'dist-img']);
-gulp.task('build-test', ['test-loader', 'test-html', 'test-img']);
 
 gulp.task('dist', ['dist-loader', 'dist-html', 'dist-img']);
-
